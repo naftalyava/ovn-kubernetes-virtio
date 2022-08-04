3156fea368558e4c2bd788d94b3ad4cefeab5154
Naftaly Avadiaev <naftaly.avadiaev@gmail.com>
2022-08-04 15:02:01 +0300

    modified scripts

--- dist/images/build.sh
+++ dist/images/build.sh
@@ -3,6 +3,10 @@
 echo "ref: $(git rev-parse  --symbolic-full-name HEAD)  commit: $(git rev-parse  HEAD)" > git_info
 
 make -C ../../go-controller
+
+echo -n "is all good?"
+read
+
 cp ../../go-controller/_output/go/bin/* .
 
 docker build -t docker.io/navadiaev/ovn-daemonset-f:latest -f Dockerfile.fedora .
